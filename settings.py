9c51bef4cc1a0f494422d68eb1598815b284eb26
Alesya Moroz <123548022+alesoid@users.noreply.github.com>
2023-11-22 11:53:09 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -22,8 +22,8 @@ SOURCES_LIST = [IMAGE, VIDEO]
 
 # Images config
 IMAGES_DIR = ROOT / 'images'
-DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
-DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
+DEFAULT_IMAGE = IMAGES_DIR / 'image_1.jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'image_2.jpg'
 
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
